b19dca2c63c1370b993143c88e088628574316eb
sergey <sergey@text.ru>
2017-11-20 14:46:34 +0700

    add pagination

--- board/views.py
+++ board/views.py
@@ -1,9 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.urls import reverse
 
+from board.forms import NewsForm
 from board.models import News
 
 
 def index(request):
     news_list = News.objects.all()
-    context = {'news_list': news_list}
+    paginator = Paginator(news_list, 20)
+    page = request.GET.get('page')
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'news_list': news}
     return render(request, 'board/index.html', context)
+
+
+def new_news(request):
+    if request.method != "POST":
+        form = NewsForm()
+    else:
+        form = NewsForm(data=request.POST)
+        if form.is_valid():
+            new_news = form.save()
+            return HttpResponseRedirect(reverse('board:index'))
+    context = {'form': form}
+    return render(request, 'board/new_news.html', context)
